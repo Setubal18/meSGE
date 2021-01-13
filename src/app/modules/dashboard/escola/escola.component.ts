0b5c781b677562bad3af07d1c91b25b24a3b6da4
Leonardo Setubal <setubal43@gmail.com>
2021-01-13 18:48:04 -0300

    feat: salvando escola

--- src/app/modules/dashboard/escola/escola.component.ts
+++ src/app/modules/dashboard/escola/escola.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
+import { EscolaService } from '../../../shared/services/escola.service';
+import { ApiResponseService } from '../../../shared/services/api-response.service';
 
 @Component({
   selector: 'app-escola',
@@ -7,14 +9,16 @@ import { FormControl, FormGroup } from '@angular/forms';
   styleUrls: ['./escola.component.scss'],
 })
 export class EscolaComponent implements OnInit {
-  public escolaForm: FormGroup
+  public escolaForm: FormGroup;
 
-  constructor() { }
+  constructor(
+    private escolaService: EscolaService,
+    private apiResponseService: ApiResponseService
+  ) { }
 
   ngOnInit() {
-    this.initform()
+    this.initform();
   }
-
 
   initform() {
     this.escolaForm = new FormGroup({
@@ -31,12 +35,17 @@ export class EscolaComponent implements OnInit {
         alameda: new FormControl(),
       }),
       turmas: new FormControl({})
-    })
+    });
   }
 
-
   save() {
-    console.log(this.escolaForm.value)
+    console.log(this.escolaForm.value);
+    this.escolaService.saveEscola(this.escolaForm.value).then(res => {
+      console.log(res);
+      this.apiResponseService.success(res);
+    }, (error) => {
+      this.apiResponseService.danger(error);
+    });
   }
 
 }
