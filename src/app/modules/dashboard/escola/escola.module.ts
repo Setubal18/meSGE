63594d221e6f0ed64e23e73e532f3d1fbcfab496
Leonardo Setubal <setubal43@gmail.com>
2021-01-13 18:53:05 -0300

    chore: realodando componente

--- src/app/modules/dashboard/escola/escola.module.ts
+++ src/app/modules/dashboard/escola/escola.module.ts
@@ -5,8 +5,8 @@ import { EscolaRoutingModule } from './escola-routing.module';
 import { EscolaComponent } from './escola.component';
 import { IonicModule } from '@ionic/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { FormIncrementalTurmasComponent } from 'src/app/shared/form-incremental-turmas/form-incremental-turmas.component';
-import { FormIncrementalTurmasModule } from '../../../shared/form-incremental-turmas/form-incremental-turmas.module';
+import { FormIncrementalTurmasComponent } from 'src/app/shared/components/form-incremental-turmas/form-incremental-turmas.component';
+import { FormIncrementalTurmasModule } from '../../../shared/components/form-incremental-turmas/form-incremental-turmas.module';
 
 
 @NgModule({
