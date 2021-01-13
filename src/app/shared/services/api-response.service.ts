b2c42dcac50628b51c80bcd258c87638f95a8b15
Leonardo Setubal <setubal43@gmail.com>
2021-01-13 18:47:35 -0300

    feat: criado api response

---
+++ src/app/shared/services/api-response.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { ToastController } from '@ionic/angular';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiResponseService {
+  private toast: any;
+  constructor(private toastController: ToastController) { }
+
+  public async success(body) {
+    this.toast = await this.toastController.create({
+      message: body.messagem,
+      color: 'success',
+      position: 'top',
+      duration: 2000,
+      animated: true
+    }).then((toastData) => {
+      console.log(toastData);
+      toastData.present();
+    });
+
+  }
+
+  public async danger(body) {
+    this.toast = await this.toastController.create({
+      message: body.error,
+      color: 'danger',
+      position: 'top',
+      duration: 2000,
+      animated: true
+    }).then((toastData) => {
+      console.log(toastData);
+      toastData.present();
+    });
+
+  }
+
+
+}
