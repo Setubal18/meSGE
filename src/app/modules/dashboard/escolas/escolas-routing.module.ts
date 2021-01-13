76426e630cff3601eb4d02f41e6e3266a42900c0
Leonardo Setubal <setubal43@gmail.com>
2021-01-12 23:32:49 -0300

    feat: adicionado listagem da escola

---
+++ src/app/modules/dashboard/escolas/escolas-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { EscolasComponent } from './escolas.component';
+
+const routes: Routes = [
+  {
+    path: '',
+    component: EscolasComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class EscolasRoutingModule { }
---
+++ src/app/modules/dashboard/escolas/escolas.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-escolas',
+  templateUrl: './escolas.component.html',
+  styleUrls: ['./escolas.component.scss'],
+})
+export class EscolasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+    console.log('escolas')
+  }
+
+}
