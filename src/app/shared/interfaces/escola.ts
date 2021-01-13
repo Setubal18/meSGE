5c6fd1568eb83279b6983110d921876160ce1374
Leonardo Setubal <setubal43@gmail.com>
2021-01-13 18:48:46 -0300

    feat: alterado local das interfaces

---
+++ src/app/shared/interfaces/escola.ts
@@ -0,0 +1,17 @@
+import { ITurma } from './turma';
+
+export interface IEscola {
+	cod: number;
+	nomeEscola: string;
+	matricula: string;
+	nomeReitor: string;
+	tel: string;
+	email: string;
+	endereco: {
+		rua: string
+		bairro: string
+		numero: string
+		alameda: string
+	};
+	turmas: ITurma[];
+}
---
+++ src/app/shared/interfaces/turma.ts
@@ -0,0 +1,6 @@
+export interface ITurma {
+	maximo_alunos: string;
+	codTurma: number;
+	responsavel: string;
+	curso: string[];
+}
